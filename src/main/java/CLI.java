99a768dee5ab024f011c4fb335db00fb25bd01fd
Tomasz Hippner <icelore@gmail.com>
2017-02-11 19:36:55 +0000

    Imorovements to CLI implementation

--- src/main/java/CLI.java
+++ src/main/java/CLI.java
@@ -7,20 +7,36 @@ public class CLI {
 
     private final String USAGE = "main [OPTIONS] -mode <query-mode> -scale <scale-factor> [Path]";
 
-    private Options options;
-    private String[] args;
+    // modes
+    private final String MODE_RANGE_ORDERS = "range-orders";
+    private final String MODE_RANGE_LINEITEM = "range-lineitem";
+    private final String MODE_JOIN_RANGE_ORDERS = "join-range-orders";
+    private final String MODE_JOIN_RANGE_LINEITEM = "join-range-lineitem";
+    private final String MODE_SAVE_PARQ = "save-parq";
 
+    // flags
+    private final String FLAG_HELP = "h";
+    private final String FLAG_MODE = "m";
+    private final String FLAG_SCALE = "s";
+    private final String FLAG_CACHE = "C";
+    private final String FLAG_PARQ = "P";
+
+
+
+    private Options options;
     private HelpFormatter helpFormatter;
     private CommandLineParser commandLineParser;
+    private CommandLine cmd;
 
 
-    public CLI(String[] args){
+    public CLI(String[] args) throws ParseException {
 
 
         options = new Options();
         addOptions();
         helpFormatter = new HelpFormatter();
         commandLineParser = new DefaultParser();
+        cmd = this.parseCommandLine(args);
 
 
 
@@ -30,21 +46,21 @@ public class CLI {
     private void addOptions() {
 
 
-        options.addOption("h", "help", false, "print help message");
-        options.addOption("P", false, "NYI - read parq rather than CSV tables");
-        options.addOption("c", "cache", false , "NYI - force cache the tables in memory");
-        options.addOption("s", "scale", false , "NYI - tables scale factor");
+        options.addOption(FLAG_HELP, "help", false, "print help message");
+        options.addOption(FLAG_PARQ, "parquet", false, "NYI - read parq rather than CSV tables");
+        options.addOption(FLAG_CACHE, "cache", false , "NYI - force cache the tables in memory");
+        options.addOption(FLAG_SCALE, "scale", false , "NYI - tables scale factor");
 
-        options.addOption(Option.builder("m")
+        options.addOption(Option.builder(FLAG_MODE)
                 .longOpt("mode")
                 .hasArg(true)
                 .required(true)
                 .desc("query modes: \n"+
-                        "range-orders - incremental 10% query series on orders table\n" +
-                        "range-lineitem - incremental 10% query series on lineitem table\n" +
-                        "join-range-orders - join with incremental 10% query series on orders table\n" +
-                        "join-range-lineitem - join with incremental 10% query series on lineitem table\n" +
-                        "save-parq - convert tables to parquett format")
+                        MODE_RANGE_ORDERS + " - incremental 10% query series on orders table\n" +
+                        MODE_RANGE_LINEITEM + " - incremental 10% query series on lineitem table\n" +
+                        MODE_JOIN_RANGE_ORDERS + " - join with incremental 10% query series on orders table\n" +
+                        MODE_JOIN_RANGE_LINEITEM + " - join with incremental 10% query series on lineitem table\n" +
+                        MODE_SAVE_PARQ + " - convert tables to parquett format")
                 .build());
 
 
@@ -52,14 +68,60 @@ public class CLI {
     }
 
 
-    public void Usage(){
+
+
+    public boolean modeIsSaveAsParq(){
+        return cmd.getOptionValue(FLAG_MODE).equals(MODE_SAVE_PARQ);
+    }
+
+    public boolean hasHelp(){
+        return cmd.hasOption(FLAG_HELP);
+    }
+
+
+    public void printUsage(){
         helpFormatter.printHelp(USAGE, options);
     }
 
 
-    public CommandLine parseCommandLine() throws ParseException {
-
+    public CommandLine parseCommandLine(String[] args) throws ParseException {
         return commandLineParser.parse(options, args);
 
+    }
+
+    public boolean modeIsRangeOrders() {
+        return cmd.getOptionValue(FLAG_MODE).equals(MODE_RANGE_ORDERS);
+    }
+
+    public boolean modeIsRangeLineitem() {
+        return cmd.getOptionValue(FLAG_MODE).equals(MODE_RANGE_LINEITEM);
+    }
+
+    public boolean modeIsJoinRangeOrders() {
+        return cmd.getOptionValue(FLAG_MODE).equals(MODE_JOIN_RANGE_ORDERS);
+    }
+
+    public boolean modeIsJoinRangeLineitem() {
+        return cmd.getOptionValue(FLAG_MODE).equals(MODE_JOIN_RANGE_LINEITEM);
+    }
+
+    public boolean hasScaleFactor() {
+        return cmd.hasOption(FLAG_SCALE);
+    }
+
+    public int getScaleFactorValue() {
+        return Integer.parseInt(cmd.getOptionValue(FLAG_SCALE));
+    }
+
+    public boolean hasPath() {
+        return cmd.getArgList().size() > 0;
+    }
+
+    public String getFilePathValue() {
+        return cmd.getArgList().get(0);
+    }
+
+    public boolean hasCache() {
+        return cmd.hasOption(FLAG_CACHE);
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,6 @@
+import org.apache.commons.cli.MissingArgumentException;
+import org.apache.commons.cli.MissingOptionException;
+import org.apache.commons.cli.ParseException;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.sql.DataFrame;
@@ -14,176 +17,249 @@ import org.apache.spark.sql.types.StructType;
 
 public class Main {
 
+    final static int REPEAT_QUERY_NUMBER = 10;
+
+    final static int DATA_MULTIPL = 100000;
+    final static int[] dataRangeFactors = {6, 12, 18, 24, 30, 36, 42, 48, 54};
+
+    final static int DEFAULT_SCALE = 1;
+    final static String DEFAULT_PATH = "/user/tomasz/db1/";
+    final static String LINEITEM = "lineitem";
+    final static String ORDERS = "orders";
+
 
 
     public static void main(String[] args) {
 
 
-        final String USAGE = "NYI";
+        try {
 
-        String file_loc = "/home/tomasz"; // default hdfs directory
+            CLI cli = new CLI(args);
+            int scaleFactor = DEFAULT_SCALE;
+            String filePath = DEFAULT_PATH;
 
-        int[] dataRanges = {600000,
-                1200000,
-                1800000,
-                2400000,
-                3000000,
-                3600000,
-                4200000,
-                4800000,
-                5400000,
-                6001215};
+            // spark setup
+            SparkConf conf = new SparkConf().setAppName("UniProject");
+            JavaSparkContext sc = new JavaSparkContext(conf);
+            //sc.addJar("/home/tomasz/UniProject.jar");
+            SQLContext sqlContext = new org.apache.spark.sql.SQLContext(sc);
 
 
-        //override default dir if passed as argument
-        if(args.length == 0){
-            // TODO: print USAGE
-            System.out.println("NYI");
-        }
-        else if(args.length > 1){
-            file_loc = args[1];
-        }
+            if(cli.hasHelp()) {
+                cli.printUsage();
+            }
 
 
+            // if contains scale factor
+            if(cli.hasScaleFactor()){
+                scaleFactor = cli.getScaleFactorValue();
+            }
 
-        // spark setup
-        SparkConf conf = new SparkConf().setAppName("UniProject"); //.setMaster("local[2]"); //@TODO  yarn or yarn-client
-        JavaSparkContext sc = new JavaSparkContext(conf);
-        SQLContext sqlContext = new org.apache.spark.sql.SQLContext(sc);
+
+            // if contains optional path
+            if(cli.hasPath()) {
+                filePath = cli.getFilePathValue();
+            }
 
 
 
+            if(cli.modeIsRangeOrders()){ // range orders
+
+                DataFrame lineitem = importLineItemTable(sqlContext,filePath);
+                DataFrame orders = importOrdersTable(sqlContext,filePath);
+                lineitem.registerTempTable(LINEITEM);
+                orders.registerTempTable(ORDERS);
+
+                // force tables into cache
+                if(cli.hasCache()){
+                    lineitem.cache();
+                    lineitem.count();
+                    orders.cache();
+                    orders.count();
+                }
+                runOrdersRanges(sc, sqlContext, scaleFactor);
+            }
+            else if(cli.modeIsRangeLineitem()) { // range lineitem
+                //TODO:
+            }
+            else if(cli.modeIsJoinRangeOrders()) { // join range orders
+                //TODO:
+            }
+            else if(cli.modeIsJoinRangeLineitem()) { // join range lineitem
+                //TODO:
+            }
+            else if (cli.modeIsSaveAsParq()){
+                // TODO: df.saveAsParquetFile("file:///home/khorm/TestGrounds/DB/lineitem.parquet");
+            }
+            else{
+                System.out.println("ERROR: Unrecognized mode!");
+                cli.printUsage();                ;
+            }
+
+            // close spark
+            sc.stop();
 
 
-
-
-        if(args[0].equals("1")){
-            // 1 range LI with join
-
-            //DataFrame lineitem = importLineItemTable(sqlContext,file_loc);
-            //DataFrame orders = importOrdersTable(sqlContext,file_loc);
-            //lineitem.registerTempTable("lineitem");
-            //orders.registerTempTable("orders");
-            // force tables into cache
-            //lineitem.cache();
-            //lineitem.count();
-            //orders.cache();
-            //orders.count();
-
-            //runOrdersJOINLineItemRangesLineItem(sc, sqlContext, dataRanges);
-            System.out.println("NYI");
-
-        }
-        else if (args[0].equals("2")){
-            // 2 range O with join
-
-            //DataFrame lineitem = importLineItemTable(sqlContext,file_loc);
-            //DataFrame orders = importOrdersTable(sqlContext,file_loc);
-            //lineitem.registerTempTable("lineitem");
-            //orders.registerTempTable("orders");
-            // force tables into cache
-            //lineitem.cache();
-            //lineitem.count();
-            //orders.cache();
-            //orders.count();
-
-            //runOrdersJOINLineItemRangesOrders(sc, sqlContext, dataRanges);
-            System.out.println("NYI");
-
-        }
-        else if (args[0].equals("3")){
-            // 3 range LI
-
-            DataFrame lineitem = importLineItemTable(sqlContext,file_loc);
-            lineitem.registerTempTable("lineitem");
-            lineitem.cache();
-            lineitem.count();
-
-
-            runLineItemRanges(sc, sqlContext, dataRanges);
-            //System.out.println("NYI");
-
-        }
-        else if (args[0].equals("4")){
-            // 4 range O
-
-            //DataFrame orders = importOrdersTable(sqlContext,file_loc);
-            //orders.registerTempTable("orders");
-            //orders.cache();
-            //orders.count();
-
-            //runOrdersRanges(sc, sqlContext, dataRanges);
-            System.out.println("NYI");
-
-        }
-        else{
-            // TODO: print USAGE
-            System.out.println("NYI");
+        } catch (MissingArgumentException e){System.out.println(e.getMessage());
+        } catch (MissingOptionException e) {System.out.println(e.getMessage());
+        } catch (ParseException e) {System.out.println(e.getMessage());
         }
 
+
+    }
+
+
+
+
+
+
+
+
+//
+//
+//        if(args[0].equals("1")){
+//            // 1 range on LineItem with join
+//
+//            DataFrame lineitem = importLineItemTable(sqlContext,file_loc);
+//            DataFrame orders = importOrdersTable(sqlContext,file_loc);
+//            lineitem.registerTempTable("lineitem");
+//            orders.registerTempTable("orders");
+//
+//            // force table into cache
+//            lineitem.cache();
+//            lineitem.count();
+//            orders.cache();
+//            orders.count();
+//
+//            runOrdersJOINLineItemRangesLineItem(sc, sqlContext);
+//
+//
+//        }
+//        else if (args[0].equals("2")){
+//            // 2 range on Order with join
+//
+//            DataFrame lineitem = importLineItemTable(sqlContext,file_loc);
+//            DataFrame orders = importOrdersTable(sqlContext,file_loc);
+//            lineitem.registerTempTable("lineitem");
+//            orders.registerTempTable("orders");
+//
+//            // force tables into cache
+//            lineitem.cache();
+//            lineitem.count();
+//            orders.cache();
+//            orders.count();
+//
+//            runOrdersJOINLineItemRangesOrders(sc, sqlContext);
+//
+//
+//        }
+//        else if (args[0].equals("3")){
+//            // 3 range on LineItem
+//
+//            DataFrame lineitem = importLineItemTable(sqlContext,file_loc);
+//            lineitem.registerTempTable("lineitem");
+//
+//            // force table into cache
+//            lineitem.cache();
+//            lineitem.count();
+//
+//            runLineItemRanges(sc, sqlContext);
+//
+//        }
+//        else if (args[0].equals("4")){
+//            // 4 range on Order
+//
+//            DataFrame orders = importOrdersTable(sqlContext,file_loc);
+//            orders.registerTempTable("orders");
+//
+//            // force table into cache
+//            orders.cache();
+//            orders.count();
+//
+//            runOrdersRanges(sc, sqlContext);
+//
+//        }
+//
 
 
 
         // save file
         //result.write().json("file:///home/khorm/TestGrounds/spark-output/out.json");
 
+
+
         // close Spark
-        sc.stop();
+        //sc.stop();
 
 
-    }
 
-    private static void runOrdersJOINLineItemRangesOrders(JavaSparkContext sc, SQLContext sqlContext, int[] dataRanges) {
+    private static void runOrdersJOINLineItemRangesOrders(JavaSparkContext sc, SQLContext sqlContext, int scaleFactor) {
 
-        for(int i = 0; i< 10; i++) {
+        for(int i = 0; i< dataRangeFactors.length; i++) { // shift range value
 
-            for(int j = 0; j<5; j++) { // repeat each range 5 times
+            for(int j = 0; j<REPEAT_QUERY_NUMBER; j++) { // repeat queries
 
-                sc.setJobGroup("TH", "Order-LineItem JOIN, Order range - " + dataRanges[i] + " (" + (i + 1) + "0%)");
-                DataFrame result = sqlContext.sql("SELECT * FROM lineitem  L JOIN orders O ON L.orderkey = O.orderkey WHERE O.orderkey < " + dataRanges[i]);
+                sc.setJobGroup("TH", "Order-LineItem JOIN, Order range - " + dataRangeFactors[i] + " (" + (i + 1) + "0%)");
+                DataFrame result = sqlContext.sql("SELECT * FROM lineitem  L JOIN orders O ON L.orderkey = O.orderkey WHERE O.orderkey < " + dataRangeFactors[i]*DATA_MULTIPL*scaleFactor);
                 result.count();
 
             }
         }
 
-
+        for(int j = 0; j<REPEAT_QUERY_NUMBER; j++) {
+            sc.setJobGroup("TH", "Order-LineItem JOIN, LineItem range - 100%");
+            DataFrame result = sqlContext.sql("SELECT * FROM lineitem  L JOIN orders O ON L.orderkey = O.orderkey");
+            result.count();
+        }
     }
 
-    private static void runOrdersJOINLineItemRangesLineItem(JavaSparkContext sc, SQLContext sqlContext, int[] dataRanges) {
 
-        for(int i = 0; i< 10; i++) {
 
-            for(int j = 0; j<5; j++) { // repeat each range 5 times
+    private static void runOrdersJOINLineItemRangesLineItem(JavaSparkContext sc, SQLContext sqlContext, int scaleFactor) {
 
-                sc.setJobGroup("TH", "Order-LineItem JOIN, LineItem range - " + dataRanges[i] + " (" + (i + 1) + "0%)");
-                DataFrame result = sqlContext.sql("SELECT * FROM lineitem  L JOIN orders O ON L.orderkey = O.orderkey WHERE L.orderkey < " + dataRanges[i]);
+        for(int i = 0; i< dataRangeFactors.length; i++) {
+
+            for(int j = 0; j<REPEAT_QUERY_NUMBER; j++) {
+
+                sc.setJobGroup("TH", "Order-LineItem JOIN, LineItem range - " + dataRangeFactors[i] + " (" + (i + 1) + "0%)");
+                DataFrame result = sqlContext.sql("SELECT * FROM lineitem  L JOIN orders O ON L.orderkey = O.orderkey WHERE L.orderkey < " + dataRangeFactors[i]*DATA_MULTIPL*scaleFactor);
                 result.count();
             }
         }
 
-
+        for(int j = 0; j<REPEAT_QUERY_NUMBER; j++) {
+            sc.setJobGroup("TH", "Order-LineItem JOIN, LineItem range - 100%");
+            DataFrame result = sqlContext.sql("SELECT * FROM lineitem  L JOIN orders O ON L.orderkey = O.orderkey");
+            result.count();
+        }
     }
 
-    private static void runLineItemRanges(JavaSparkContext sc, SQLContext sqlContext, int[] dataRanges) {
 
-        for(int i = 0; i< 10; i++){
 
-            sc.setJobGroup("TH", "LineItem - " + dataRanges[i] + " (" + (i+1) + "0%)");
-            DataFrame result = sqlContext.sql("SELECT * FROM lineitem WHERE orderkey < " + dataRanges[i]);
+    private static void runLineItemRanges(JavaSparkContext sc, SQLContext sqlContext, int scaleFactor) {
+
+        for(int i = 0; i< dataRangeFactors.length; i++){
+
+            sc.setJobGroup("TH", "LineItem - " + dataRangeFactors[i] + " (" + (i+1) + "0%)");
+            DataFrame result = sqlContext.sql("SELECT * FROM lineitem WHERE orderkey < " + dataRangeFactors[i]*DATA_MULTIPL*scaleFactor);
             result.count();
         }
 
     }
 
-    private static void runOrdersRanges(JavaSparkContext sc, SQLContext sqlContext, int[] dataRanges) {
+
+
+    private static void runOrdersRanges(JavaSparkContext sc, SQLContext sqlContext, int scaleFactor) {
         // Orders 10 to 100% range
-        for(int i = 0; i< 10; i++){
+        for(int i = 0; i< dataRangeFactors.length; i++){
 
-            sc.setJobGroup("TH", "Orders - " + dataRanges[i] + " (" + (i+1) + "0%)");
-            DataFrame result = sqlContext.sql("SELECT * FROM orders WHERE orderkey < " + dataRanges[i]);
+            sc.setJobGroup("TH", "Orders - " + dataRangeFactors[i] + " (" + (i+1) + "0%)");
+            DataFrame result = sqlContext.sql("SELECT * FROM orders WHERE orderkey < " + dataRangeFactors[i]*DATA_MULTIPL*scaleFactor);
             result.count();
         }
     }
+
+
 
     private static DataFrame importLineItemTable(SQLContext sqlContext, String file_loc) {
 
@@ -238,15 +314,14 @@ public class Main {
                 .schema(customSchema)
                 .option("delimiter", "|")
                 .option("dateFormat", "YYYY-MM-DD")
-                .load(file_loc +  "/lineitem.tbl"); // @TODO  change
-
-
-        //df.saveAsParquetFile("file:///home/khorm/TestGrounds/DB/lineitem.parquet");
+                .load(file_loc +  "lineitem.tbl");
 
        return df;
     }
 
-    private static DataFrame importOrdersTable(SQLContext sqlContext){
+
+
+    private static DataFrame importOrdersTable(SQLContext sqlContext, String file_loc){
 
         //example
         // 1|36901|O|173665.47|1996-01-02|5-LOW|Clerk#000000951|0|nstructions sleep furiously among |
@@ -286,10 +361,8 @@ public class Main {
                 .schema(customSchema)
                 .option("delimiter", "|")
                 .option("dateFormat", "YYYY-MM-DD")
-                .load("file:///home/khorm/TestGrounds/DB/orders.tbl"); // @TODO  change
+                .load(file_loc +  "orders.tbl");
 
-
-        //df.saveAsParquetFile("file:///home/khorm/TestGrounds/DB/orders.parquet");
         return df;
 
     }
